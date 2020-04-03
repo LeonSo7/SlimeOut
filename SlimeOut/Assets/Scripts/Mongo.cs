914aa2b57c18ab5ffc4b76616652a44429005fa8
tiwarim <tiwarim@mcmaster.ca>
2020-04-03 12:15:58 -0400

    adding start scene and trying to get MongoDB working

---
+++ SlimeOut/Assets/Scripts/Mongo.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using UnityEngine;
+
+public class Mongo
+{
+    private const string MONGO_URI = "";
+    private const string DATABASE_NAME = "";
+
+    private MongoClient client;
+    private MongoServer server;
+    private MongoDatabase db;
+
+    private void Init()
+    {
+        client = new MongoClient(MONGO_URI);
+        server = client.GetServer();
+        db = server.GetDatabase(DATABASE_NAME);
+
+        // This is where we would initialize collections
+
+        Debug.Log("Databse has been initialized");
+    }
+
+    public void Shutdown()
+    {
+        client = null;
+        server.Shutdown();
+        db = null;
+    }
+}
