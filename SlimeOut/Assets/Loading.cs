397a133dec13e555f3ef68e9539ad264f1a406bf
unknown <caoh8@mcmaster.ca>
2020-04-07 13:56:21 -0400

    update battle select and loading nav

---
+++ SlimeOut/Assets/Loading.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        load();
+    }
+
+    IEnumerator load()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("InBattleScene");
+    }
+    // Update is called once per frame
+}
