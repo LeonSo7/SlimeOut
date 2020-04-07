1daf727ac47e35077bbb210a0bcf7f9c26f7a9be
Matthew-dw <matthew.rc3@gmail.com>
2020-04-07 15:22:16 -0400

    updated item descriptions

--- SlimeOut/Assets/Scripts/Inventory/Item.cs
+++ SlimeOut/Assets/Scripts/Inventory/Item.cs
@@ -95,18 +95,18 @@ public class Item
     public string GetDescription() {
         switch (type) {
             default:
-            case ItemType.blue1:    return "blue food level 1";
-            case ItemType.blue2:    return "blue food level 2";
-            case ItemType.blue3:    return "blue food level 3";
-            case ItemType.red1:     return "red food level 1";
-            case ItemType.red2:     return "red food level 2";
-            case ItemType.red3:     return "red food level 3";
-            case ItemType.green1:   return "green food level 1";
-            case ItemType.green2:   return "green food level 2";
-            case ItemType.green3:   return "green food level 3";
-            case ItemType.xp1:       return "exp level 1";
-            case ItemType.xp2:       return "exp level 2";
-            case ItemType.xp3:       return "exp food level 3";
+            case ItemType.blue1:    return "Lesser Blue food. Adds 5 to hunger.";
+            case ItemType.blue2:    return "Blue food. Adds 10 to hunger.";
+            case ItemType.blue3:    return "Greater Blue food. Adds 20 to hunger.";
+            case ItemType.red1:     return "Lesser Red food. Adds 5 to hunger.";
+            case ItemType.red2:     return "Red food. Adds 10 to hunger.";
+            case ItemType.red3:     return "Greater Red food. Adds 20 to hunger.";
+            case ItemType.green1:   return "Lesser Green food. Adds 5 to hunger.";
+            case ItemType.green2:   return "Green food. Adds 10 to hunger.";
+            case ItemType.green3:   return "Greater Green food. Adds 20 to hunger.";
+            case ItemType.xp1:       return "Lesser Experience Potion. Adds 5 to exp.";
+            case ItemType.xp2:       return "Experience Potion. Adds 10 to exp.";
+            case ItemType.xp3:       return "Greater Experience Potion. Adds 20 to exp.";
         }
     }
 }
