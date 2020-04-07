14314139f1d74168afbb217ee99d3dcc36c814e4
Marlee Roth <Marlee.roth@gmail.com>
2020-04-07 13:54:20 -0400

    Added saved popup

---
+++ SlimeOut/Assets/Scripts/Popup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    public void OpenPanel(){
+        // SAVE FUNCTION
+        if(Panel != null){
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);
+        }
+    }
+}
